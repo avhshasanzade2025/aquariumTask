7f1eaed816ae127a5603f86e4c507fc1af2870bb
Seadet <seadet.hesenzade2000@gmail.com>
2025-04-22 08:35:17 +0400

    commit

--- app.js
+++ app.js
@@ -1,62 +1,100 @@
 $(document).ready(function () {
+  // Select the fish elements and aquarium container
   const fishElements = $(".fish");
   const aquarium = $(".aquarium");
-  const controlledFish = $("#controlledFish");
   const fishes = [];
+
+  // Select the control buttons
+  const controls = {
+    up: $("#up"),
+    down: $("#down"),
+    left: $("#left"),
+    right: $("#right"),
+  };
 
   fishElements.each(function () {
     const fish = $(this);
-    //Get position and size
+
+    // Set random position for each fish
     const width = fish.width();
     const height = fish.height();
     const x = Math.random() * (aquarium.width() - width);
     const y = Math.random() * (aquarium.height() - height);
 
+    // Set default position for the fish
     fish.css({
       left: x + "px",
       top: y + "px",
     });
 
+    // Set random speed and direction for each fish
     const fishData = {
       element: fish,
-      speedX: 1,
-      speedY: 1,
       x: x,
       y: y,
       width: width,
       height: height,
       directionX: Math.random() > 0.5 ? 1 : -1,
       directionY: Math.random() > 0.5 ? 1 : -1,
+      isControlled: fish.attr("id") === "controlledFish",
     };
-
-    if (fishData.directionX === -1) {
-      fish.addClass("flip-horizontal");
-    }
-
     fishes.push(fishData);
 
+    // Flip the fish if it is moving left
+    if (fishData.directionX === -1) {
+      fish.addClass("flipped");
+    }
+
+    // Create a bubble every second for every fish
     setInterval(() => {
       createBubble(fish);
     }, 1000);
   });
 
-  //   controlledFish.on("keydown", function (event) {
-  //     const key = event.key;
-  //     const fish = $(this);
+  controls.up.on("click", function () {
+    changeDirection("up");
+  });
 
-  //     switch (key) {
-  //       case "ArrowUp":
-  //         fish.directionY = 1;
-  //         // fish.css("top", fish.position().top - speed + "px");
-  //         break;
+  controls.down.on("click", function () {
+    changeDirection("down");
+  });
 
-  //     }
-  //   });
+  controls.left.on("click", function () {
+    changeDirection("left");
+  });
 
+  controls.right.on("click", function () {
+    changeDirection("right");
+  });
+
+  // Change the direction of the controlled fish based on button clicks
+  function changeDirection(direction) {
+    const controlledFish = fishes.find((fish) => fish.isControlled);
+    if (!controlledFish) return;
+
+    switch (direction) {
+      case "up":
+        controlledFish.directionY = -1;
+        break;
+      case "down":
+        controlledFish.directionY = 1;
+        break;
+      case "left":
+        controlledFish.directionX = -1;
+        controlledFish.element.addClass("flipped");
+        break;
+      case "right":
+        controlledFish.directionX = 1;
+        controlledFish.element.removeClass("flipped");
+        break;
+    }
+  }
+
+  // Create a bubble every second for every fish
   function createBubble(element) {
     const bubble = $("<div>").addClass("bubble");
 
-    // CReate bubbles with random sizes
+    // Create bubbles with random sizes
     const size = Math.floor(Math.random() * 10) + 8;
     bubble.css({
       width: size + "px",
@@ -82,32 +120,37 @@ $(document).ready(function () {
     element.append(bubble);
   }
 
+  // Function to animate the fish swimming in the aquarium
   function swim() {
     fishes.forEach((fish) => {
-      fish.x += fish.speedX * fish.directionX;
-      fish.y += fish.speedY * fish.directionY;
+      fish.x += fish.directionX;
+      fish.y += fish.directionY;
 
+      //Set direction vertically
       if (fish.x <= 0) {
         fish.directionX = 1;
-        fish.element.removeClass("flip-horizontal");
+        fish.element.removeClass("flipped");
       } else if (fish.x + fish.width >= aquarium.width()) {
         fish.directionX = -1;
-        fish.element.addClass("flip-horizontal");
+        fish.element.addClass("flipped");
       }
+
+      //Set direction horizontally
       if (fish.y <= 0) {
         fish.directionY = 1;
       } else if (fish.y + fish.height >= aquarium.height()) {
         fish.directionY = -1;
       }
+      console.log(fish.x);
       fish.element.css({
         left: fish.x + "px",
         top: fish.y + "px",
       });
     });
-    requestAnimationFrame(swim);
+    // requestAnimationFrame(swim);
   }
-  function checkCollision() {
-    //how to check if 2 or more objects collide with each other
-  }
+
+  // Check collisions between fish
+
   swim();
 });
